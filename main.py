a20df9f04390934af55001cdb8e6b14b50626423
Jim deVos <jtdevos@asu.edu>
2024-09-11 16:27:22 -0700

    added stub for diff_filter function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from skimage.metrics import structural_similarity as ssim
+from pathlib import Path
+
 import sys
 import cv2
 
@@ -16,19 +18,46 @@ def measure_similarity(image1_path, image2_path):
 def imgdiff(file1, file2):
     similarity = measure_similarity(file1, file2)
     print(f"Image similarity (SSIM): {similarity}")
+    return similarity
+
+def diff_filter(imgpath, dmin):
+    """
+    Given a path containing images in sequential order,  only return the paths of the files
+    are less that the minimum similarity from the preceeding file. 
+    - For each comparison there will be a "file1" and "file2" .
+    - if imgdiff(file1, file2) < dmin, output the path of file1, and then set 
+        file2 to be the next file1.  
+    - continue the process until all files processed.
+
+    """
+    # get list of files
+    paths = [filepath for filepath in sorted(Path(imgpath).iterdir())]
+    for path in paths:
+        print(path)
 
 def getargs():
     file1 = 'file1.jpg'
     file2 = 'file2.jpg'
     if(len(sys.argv) >= 3):
         _,  file1, file2, *_ = sys.argv
-    print(f'file1: {file1}')
-    print(f'file2: {file2}')
+    # print(f'file1: {file1}')
+    # print(f'file2: {file2}')
     return file1, file2
 
 def main():
     file1, file2 = getargs()
     imgdiff(file1, file2)
 
+def main2(): 
+    print('you called main2')
+
+    print(f'arg length is {len(sys.argv)}')
+    if len(sys.argv) < 2:
+        print('not enough args')
+        exit(1)
+    filepath = sys.argv[1]
+    diff_filter(filepath, 0.5)
+
+
 if __name__ == '__main__':
-    main()
+    main2()
